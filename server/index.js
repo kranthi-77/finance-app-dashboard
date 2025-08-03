f9aaa2fba2d69c32a5ececdfdbfbb51ff0ea62a5
kranthi-77 <vkranthi485@gmail.com>
2025-08-03 11:52:10 +0530

    commit

--- server/index.js
+++ server/index.js
@@ -22,7 +22,7 @@ app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use(morgan("common"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cors());
+app.use(cors())
 
 /* ROUTES */
 app.use("/kpi", kpiRoutes);
